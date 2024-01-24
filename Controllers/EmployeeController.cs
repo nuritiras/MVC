b85650ec894e21646b43a00d66319a5d2450ced0
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2024-01-24 11:51:16 +0300

    first commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class EmployeeController:Controller
+    {
+        public String Index()
+        {
+            return "Hello World.";
+        }
+        public ViewResult Index2()
+        {
+            return View("Index");
+        }
+        public IActionResult Index3()
+        {
+            return Content("Employee");
+        }
+    }
+}
